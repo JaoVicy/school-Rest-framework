a30623f3a1dd691465ad6a74f88fa84ac7a487e5
JaoVicy <filipimjoa@gmail.com>
2025-08-17 21:39:34 -0300

    chore: added def

--- escola/views.py
+++ escola/views.py
@@ -1,1 +1,11 @@
 from django.http import JsonResponse
+
+def estudantes(request):
+    if request.method == 'GET':
+        estudante = {
+            "id": 1,
+            "nome": "João da Silva",
+            "idade": 20,
+            "curso": "Engenharia de Software"
+        }
+        return JsonResponse(estudante)
