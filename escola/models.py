2139f0b1a0b7255c3c83421a6628e571a30c1a0c
JaoVicy <filipimjoa@gmail.com>
2025-08-24 21:48:37 -0300

    chore: added max_length fild

--- escola/models.py
+++ escola/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Estudante(models.Model):
-    nome = models.CharField()
+    nome = models.CharField(max_length=100)
     idade = models.IntegerField()
     email = models.EmailField()
 
