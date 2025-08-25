9ab9cc65f15f33b29a0ae46f07f050ea9fc45882
JaoVicy <filipimjoa@gmail.com>
2025-08-24 21:50:48 -0300

    feat: added cpf

--- escola/models.py
+++ escola/models.py
@@ -6,6 +6,7 @@ class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField()
     email = models.EmailField(blank = False)
+    cpf = models.CharField(max_length=11, unique=True)
 
     def __str__(self):
         return self.nome
