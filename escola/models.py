0040c001eb21aa09904c70281bc21cb9e8a4107f
JaoVicy <filipimjoa@gmail.com>
2025-08-24 21:48:03 -0300

    feat: added Estudante model

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudante(models.Model):
+    nome = models.CharField()
+    idade = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
