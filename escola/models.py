f41429ebb6d5ed7d61117bd6102b7460f1e81d85
JaoVicy <filipimjoa@gmail.com>
2025-08-24 21:54:41 -0300

    feat: created Curso model

--- escola/models.py
+++ escola/models.py
@@ -12,3 +12,13 @@ class Estudante(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    carga_horaria = models.IntegerField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
