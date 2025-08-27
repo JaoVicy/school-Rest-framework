09db932dca5d59535daf0f9fefae6da43103881c
JaoVicy <filipimjoa@gmail.com>
2025-08-27 20:32:15 -0300

    feat: created EstudanteSerializer

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Estudante, Curso
+
+class EstudanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estudante
+        field = ['id', 'nome', 'idade', 'email', 'cpf', 'data_nacimento', 'celular']
