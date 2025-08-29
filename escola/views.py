ece8664b4402ef2ee203adc6fd22f410b9181a77
JaoVicy <filipimjoa@gmail.com>
2025-08-29 13:31:22 -0300

    feat: created viewset

--- escola/views.py
+++ escola/views.py
@@ -5,3 +5,7 @@ from rest_framework import viewsets
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
