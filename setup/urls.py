9e4ea1336b30a497f8be8e41658df40882381f24
JaoVicy <filipimjoa@gmail.com>
2025-09-02 13:16:15 -0300

    feat: created router

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register("estudantes", EstudanteViewSet, basename="Estudante") 
 router.register("cursos", CursoViewSet, basename="Cursos")
+router.register("matriculas", MatriculaViewSet, basename="Matriculas")
 
 
 urlpatterns = [
