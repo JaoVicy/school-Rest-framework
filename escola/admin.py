a4a04d8f1fecbf36f5fb4609a4d9bc4cb5c439f6
JaoVicy <filipimjoa@gmail.com>
2025-09-02 13:18:56 -0300

    feat: created Matriculas in admin

--- escola/admin.py
+++ escola/admin.py
@@ -18,5 +18,10 @@ class Cursos(admin.ModelAdmin):
     list_per_page = 20
     search_fields = ('codigo',)
 
-
 admin.site.register(Curso, Cursos)
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'estudante', 'curso', 'periodo',)
+    list_display_links = ('id', )
+
+admin.site.register(Matricula, Matriculas)
