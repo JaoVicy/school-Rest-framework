06df4b3e0e5d64e068ab4b927ed761468232ad5e
JaoVicy <filipimjoa@gmail.com>
2025-09-02 13:32:37 -0300

    feat: created serializer

--- escola/serializers.py
+++ escola/serializers.py
@@ -15,3 +15,13 @@ class MatriculaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Matricula
         exclude = []
+
+class ListMatriculasEstudanteSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.ReadOnlyField() #Campo referente ao get_periodo_display
+
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj): # A def esta referênciando o campo periodo, o get_ + nome do campo, é a convenção
+        return obj.get_periodo_display() # Pega o valor legível do campo periodo (com o uso do display)
