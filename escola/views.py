16b85acf3b6f595706ae674b3c0a9a73440e954a
JaoVicy <filipimjoa@gmail.com>
2025-09-02 13:49:35 -0300

    chore: created viewSet

--- escola/views.py
+++ escola/views.py
@@ -19,3 +19,9 @@ class ListaMatriculaEstudante(generics.ListAPIView):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']) # Filtra as matrículas pelo ID do estudante
         return queryset
     serializer_class = ListMatriculasEstudanteSerializer
+
+class ListaMatriculasCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']) # Filtra as matrículas pelo ID do curso
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
