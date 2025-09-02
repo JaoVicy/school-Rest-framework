3cea4ff9c9ea8b612efa98fa337090f88016a185
JaoVicy <filipimjoa@gmail.com>
2025-09-02 13:13:43 -0300

    chore: added import

--- escola/views.py
+++ escola/views.py
@@ -1,5 +1,5 @@
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer
 from rest_framework import viewsets
 
 class EstudanteViewSet(viewsets.ModelViewSet):
