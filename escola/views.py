4a8c6b2c4c69fb4032957371ff41c7c050ec5664
JaoVicy <filipimjoa@gmail.com>
2025-09-04 13:17:21 -0300

    perf: removing unitary authentications

--- escola/views.py
+++ escola/views.py
@@ -5,34 +5,34 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAdminUser] # Apenas administradores podem acessar
+#    authentication_classes = [BasicAuthentication]
+#    permission_classes = [IsAdminUser] # Apenas administradores podem acessar
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+#    authentication_classes = [BasicAuthentication]
+#    permission_classes = [IsAuthenticated]
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly] # Qualquer usuário autenticado pode criar, mas apenas leitura para não autenticados
+#    authentication_classes = [BasicAuthentication]
+#    permission_classes = [IsAuthenticatedOrReadOnly] # Qualquer usuário autenticado pode criar, mas apenas leitura para não autenticados
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
 class ListaMatriculaEstudante(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+#    authentication_classes = [BasicAuthentication]
+#    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']) # Filtra as matrículas pelo ID do estudante
         return queryset
     serializer_class = ListMatriculasEstudanteSerializer
 
 class ListaMatriculaCurso(generics.ListAPIView):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+#    authentication_classes = [BasicAuthentication]
+#    permission_classes = [IsAuthenticated]
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']) # Filtra as matrículas pelo ID do curso
         return queryset
