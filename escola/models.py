be36d09debd5372e4b566283bc412770c1fe8de2
JaoVicy <filipimjoa@gmail.com>
2025-09-08 13:34:13 -0300

    chore: addm coment for unique

--- escola/models.py
+++ escola/models.py
@@ -6,7 +6,7 @@ class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField()
     email = models.EmailField(blank = False)
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=11, unique=True) # CPF deve ser único
     data_nacimento = models.DateField(null=True, blank=True)
     celular = models.CharField(max_length=14)
 
