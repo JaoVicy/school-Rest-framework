458ff3e74855c7861a45256e9befaefefccfa5cd
JaoVicy <filipimjoa@gmail.com>
2025-09-10 22:11:34 -0300

    chore: added Versioning in REST_FRAMEWORK

--- setup/settings.py
+++ setup/settings.py
@@ -135,4 +135,6 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
+
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
 }
